c988c6c408d645d15f8cc6276d13b92f1c7e9417
tenenwurcel <tenenwurcel@gmail.com>
2023-02-06 17:58:46 -0300

    added with logger config - gocore

--- gocore/bench_test.go
+++ gocore/bench_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/dock-tech/dock-gocore-lib/log"
+	"github.com/dock-tech/dock-gocore-lib/logger"
 	"io"
 	"testing"
 )
@@ -18,6 +19,26 @@ func BenchmarkGocoreWith10Fields(b *testing.B) {
 			Age:     42,
 			Field1:  "value1",
 			Field2:  "value2",
+		})
+	}
+}
+
+func BenchmarkGocoreWith10FieldsWithLoggerConfig(b *testing.B) {
+	appName := "dummy"
+	appVersion := "0.0.1"
+	env := "DEV"
+
+	log.SetLevel(log.DebugLevel)
+	log.InitializeLogs(log.Fields{"app_name": appName, "app_version": appVersion})
+	logger.Config("panic: ", appName, appVersion, env)
+	log.SetOutput(io.Discard)
+
+	for n := 0; n < b.N; n++ {
+		log.Debugj(Fields{
+			Message: "Hello Worlds",
+			Name:    "John Doe",
+			Age:     42,
+			Field1:  "value1",
 		})
 	}
 }
